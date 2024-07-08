b558fa1e9a5d2945baaffe2567035717c1a52713
FizzyApple12 <github@fizzyapple12.com>
2024-07-07 22:01:35 -0400

    More audio system initialization work

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,86 +1,141 @@
-use cpal::{traits::{DeviceTrait, HostTrait}, Device, Host, SupportedStreamConfig};
+use cpal::{
+    traits::{DeviceTrait, HostTrait}, Device, Host, Stream, StreamConfig
+};
 
 #[derive(PartialEq)]
-enum CPALState {
-    NotReady = 0,
-    HostReady = 1,
-    HostDeviceReady = 2,
-    HostDeviceConfigReady = 3,
+enum CPALReadyState {
+    Host = 1,
+    HostDevice = 2,
+    HostDeviceConfig = 3,
+    HostDeviceConfigStream = 4,
 }
 
 pub struct AudioSystem {
-    cpal_state: CPALState,
+    cpal_state: CPALReadyState,
     cpal_host: Host,
     cpal_device: Option<Device>,
-    cpal_config: Option<SupportedStreamConfig>
+    cpal_config: Option<StreamConfig>,
+    cpal_stream: Option<Stream>
 }
 
 pub fn create() -> AudioSystem {
     let cpal_host = cpal::default_host();
 
     let mut audio_system = AudioSystem {
-        cpal_state: CPALState::HostReady,
+        cpal_state: CPALReadyState::Host,
         cpal_host,
         cpal_device: None,
         cpal_config: None,
+        cpal_stream: None
     };
 
     audio_system.prepare();
 
-    return audio_system;
+    audio_system
 }
 
 impl AudioSystem {
     pub fn prepare(&mut self) -> bool {
-        let new_device;
-        if self.cpal_state == CPALState::HostReady {
-            new_device = self.cpal_host.default_output_device();
+        let new_device = if self.cpal_state == CPALReadyState::Host {
+            self.cpal_host.default_output_device()
         } else {
-            new_device = self.cpal_device.clone();
-        }
+            self.cpal_device.clone()
+        };
 
         match new_device {
             Some(device) => {
                 self.cpal_device = Some(device);
 
-                if self.cpal_state == CPALState::HostReady {
-                    self.cpal_state = CPALState::HostDeviceReady;
+                if self.cpal_state == CPALReadyState::Host {
+                    self.cpal_state = CPALReadyState::HostDevice;
                 }
 
-                match device.clone().supported_output_configs() {
-                    Ok(mut supported_configs_range) => {
-                        match supported_configs_range.next() {
-                            Some(supported_config) => {
-                                self.cpal_config = Some(supported_config.with_max_sample_rate());
+                let new_config = if self.cpal_state == CPALReadyState::HostDevice {
+                    match self
+                        .cpal_device
+                        .as_ref()
+                        .expect(
+                            "ummm hi, if you see this, all logic has been defied on the first case",
+                        )
+                        .supported_output_configs()
+                    {
+                        Ok(mut supported_configs_range) => {
+                            supported_configs_range.next().map(|supported_config| {
+                                supported_config.with_max_sample_rate().config()
+                            })
+                        }
+                        Err(_) => None,
+                    }
+                } else {
+                    self.cpal_config.clone()
+                };
 
-                                if self.cpal_state == CPALState::HostDeviceReady {
-                                    self.cpal_state = CPALState::HostDeviceConfigReady;
+                match new_config {
+                    Some(config) => {
+                        self.cpal_config = Some(config);
+
+                        if self.cpal_state == CPALReadyState::HostDevice {
+                            self.cpal_state = CPALReadyState::HostDeviceConfig;
+                        }
+
+                        let new_stream = if self.cpal_state == CPALReadyState::HostDeviceConfig {
+                            match self.cpal_device.as_ref().expect("ummm hi, if you see this, all logic has been defied on the second case").build_output_stream(
+                                self.cpal_config.as_ref().expect("ummm hi, if you see this, all logic has been defied on the third case"),
+                                |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                                    AudioSystem::stream_data_callback(self, );
+                                },
+                                move |err| {
+                                    // react to errors here.
+                                },
+                                None // None=blocking, Some(Duration)=timeout
+                            ) {
+                                Ok(stream) => Some(stream),
+                                Err(_) => None,
+                            }
+                        } else {
+                            self.cpal_stream
+                        };
+
+                        match new_stream {
+                            Some(stream) => {
+                                self.cpal_stream = Some(stream);
+
+                                if self.cpal_state == CPALReadyState::HostDeviceConfig {
+                                    self.cpal_state = CPALReadyState::HostDeviceConfigStream;
                                 }
 
                                 true
-                            },
+                            }
                             None => {
-                                self.cpal_state = CPALState::HostDeviceReady;
+                                self.cpal_state = CPALReadyState::HostDeviceConfig;
                                 self.cpal_config = None;
 
                                 false
                             }
                         }
                     }
-                    Err(_) => {
-                        self.cpal_state = CPALState::HostDeviceReady;
+                    None => {
+                        self.cpal_state = CPALReadyState::HostDevice;
                         self.cpal_config = None;
 
                         false
                     }
                 }
-            },
+            }
             None => {
-                self.cpal_state = CPALState::HostReady;
+                self.cpal_state = CPALReadyState::Host;
                 self.cpal_device = None;
 
                 false
             }
         }
+    }
+
+    fn stream_data_callback(&mut self) {
+        
+    }
+
+    fn stream_error_callback(&mut self) {
+        
     }
 }
